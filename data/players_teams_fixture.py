c91850f776084f70ab66293a91755783b10e6de7
[blake edmond] <[blake.edmond9@gmail.com]>
2021-06-17 13:38:39 -0400

    repackaged application, moving venv to project root dir

---
+++ data/players_teams_fixture.py
@@ -0,0 +1,47 @@
+#### Stats for every player in every game of AFL since 2012 ####
+# format columns:
+def format_df(df):
+    df.columns = [c.replace(' ', '_') for c in df.columns]
+    df.columns = [c.replace('(mm)', '') for c in df.columns]
+    df.Player = df.Player.str.replace(', ', '_')
+    df.Round = df.Round.str.replace('R', '').astype(int)
+
+# isolate first and last names of each player and assign to respective columns:
+def first_last_names(df):    
+    pattern = r'^([A-Z][a-z]+)_([A-Z][a-z]+)$'
+    names = df.Player.str.split(r'_', expand=True).rename({0: 'Last_Name', 1: 'First_Name'}, axis=1)
+    df.insert(df.columns.get_loc('DOB'), 'First_Name', names.First_Name)
+    df.insert(df.columns.get_loc('DOB'), 'Last_Name', names.Last_Name)
+
+# read in dataset of AFL player statistics and format:
+afl = pd.read_csv(r'data/afl_stats.csv')
+afl = afl.loc[afl.Year == 2020].loc[afl.Round != 'EF'].loc[afl.Round != 'PF'].loc[afl.Round != 'SF'].loc[afl.Round != 'QF'].loc[afl.Round != 'GF']
+format_df(afl)
+first_last_names(afl)
+
+# isolate players from each team:
+teams = pd.pivot_table(afl, values=[
+                                    
+       'Height', 'Weight', 'Disposals', 'Kicks', 'Marks', 'Handballs', 'Goals',
+       'Behinds', 'Hit_Outs', 'Tackles', 'Rebounds', 'Inside_50s',
+       'Clearances', 'Clangers', 'Frees', 'Frees_Against', 'Brownlow_Votes',
+       'Contested_Possessions', 'Uncontested_Possessions', 'Contested_Marks',
+       'Marks_Inside_50', 'One_Percenters', 'Bounces', 'Goal_Assists'
+
+       ], index=['Team', 'Player', 'First_Name', 'Last_Name'])\
+          .reset_index()\
+          .reindex(columns=[
+                
+       'Player', 'First_Name', 'Last_Name', 'Team', 'Height', 'Weight', 
+       'Disposals', 'Kicks', 'Marks', 'Handballs', 'Goals',
+       'Behinds', 'Hit_Outs', 'Tackles', 'Rebounds', 'Inside_50s',
+       'Clearances', 'Clangers', 'Frees', 'Frees_Against', 'Brownlow_Votes',
+       'Contested_Possessions', 'Uncontested_Possessions', 'Contested_Marks',
+       'Marks_Inside_50', 'One_Percenters', 'Bounces', 'Goal_Assists'
+       
+       ]).round(2)
+
+# pivot DF to get access to individual game characteristics:
+pivotafl = afl.loc[afl.Year == 2020].pivot(index=['Round', 'Team', 'Player'], columns='Opposition')\
+              .stack().reset_index()\
+              .set_index(['Round', 'Team', 'Opposition', 'Day', 'Date', 'Start_Time', 'Venue', 'Attendance', 'Rainfall'])
---
+++ data/stadium_weather.py
@@ -0,0 +1,54 @@
+# scrape current afl stadium data and construct DF:
+stadiums = pd.read_html('https://en.wikipedia.org/wiki/List_of_Australian_Football_League_grounds')[0]\
+             .drop(index=[14,15], columns=['Image', 'Other/sponsored names', 'State/territory', 'First used'])
+
+# present stadium names in the form to match afl_stats.csv:
+stad_mapper = {
+      'Melbourne Cricket Ground' : 'M.C.G.',
+      'Docklands Stadium' : 'Docklands',
+      'Sydney Cricket Ground' : 'S.C.G.',
+      'The Gabba' : 'Gabba',
+      'Carrara Stadium' : 'Carrara',
+      'Sydney Showground Stadium' : 'Sydney Showground'
+}
+stadiums.Ground = stadiums.Ground.replace(stad_mapper)
+
+# format values:
+stadiums.Capacity = [re.sub(r'\[.+\]', '', vals) for vals in stadiums.Capacity]
+stadiums['Current tenant(s)'] = [re.sub(r'\[.+\]', '', vals) for vals in stadiums['Current tenant(s)']]
+stadiums['Current tenant(s)'] = [re.sub(r"([a-z])([A-Z])", r"\1, \2", tenant).split(', ') for tenant in stadiums['Current tenant(s)']]
+stadiums.Capacity = stadiums.Capacity.str.replace(',', '').astype(int)
+stadiums.iat[5,3] = ['Brisbane Lions']
+
+# isolate the weather for each Stadium location as a DF:
+weather_urls = {
+    'Wendouree' : 'https://www.eldersweather.com.au/climate-history/vic/wendouree',
+    'Melbourne' : 'https://www.eldersweather.com.au/climate-history/vic/melbourne',
+    'Perth' : 'https://www.eldersweather.com.au/climate-history/wa/perth',
+    'Adelaide' : 'https://www.eldersweather.com.au/climate-history/sa/adelaide',
+    'Sydney' : 'https://www.eldersweather.com.au/climate-history/nsw/sydney',
+    'Brisbane' : 'https://www.eldersweather.com.au/climate-history/qld/brisbane',
+    'Geelong' : 'https://www.eldersweather.com.au/climate-history/vic/geelong',
+    'Gold Coast' : 'https://www.eldersweather.com.au/climate-history/qld/coolangatta',
+    'Launceston' : 'https://www.eldersweather.com.au/climate-history/tas/launceston',
+    'Hobart' : 'https://www.eldersweather.com.au/climate-history/tas/hobart',
+    'Canberra' : 'https://www.eldersweather.com.au/climate-history/act/canberra',
+    'Darwin' : 'https://www.eldersweather.com.au/climate-history/nt/darwin',
+    'Alice Springs' : 'https://www.eldersweather.com.au/climate-history/nt/alice-springs'
+}
+
+# format _weather DFs attached to each Stadium object:
+weather_mapper = {
+    'Jan' : 1,
+    'Feb' : 2,
+    'Mar' : 3,
+    'Apr' : 4,
+    'May' : 5,
+    'Jun' : 6,
+    'Jul' : 7,
+    'Aug' : 8,
+    'Sep' : 9,
+    'Oct' : 10,
+    'Nov' : 11,
+    'Dec' : 12
+}
